0aaa84aa96fcc7a112d41405c614fd6788ef80dc
Teodor Perunicic <perunicic.teodor@jjzmaj.edu.rs>
2021-01-13 12:15:59 +0100

    added rsi
    added rsi

--- stock.py
+++ stock.py
@@ -9,15 +9,15 @@ from datetime import datetime
 import matplotlib
 
 class Option:
-    def __init__(self, symbol, price, data_earnings, open_position):
+    def __init__(self, symbol, price):
         self.symbol = symbol
         self.price = price
         self.data_stock_price = pd.DataFrame()
-        self.data_earnings = data_earnings
+        self.data_earnings = pd.DataFrame()
         self.open_position = False
     
     def scrape_data(self):
-        stock = pdr.get_data_yahoo(self.symbol,start = datetime(2010, 6, 29), end = datetime(2021,1,12))
+        stock = pdr.get_data_yahoo(self.symbol,start = datetime(2010, 6, 29), end = datetime.today())
         self.data_stock_price = pd.DataFrame(stock)
         self.data_stock_price.to_excel('D:\Code\Python\Trading Algorithm\ '+self.symbol+'.xlsx')
         #scrape data from yahoo finace
@@ -25,21 +25,40 @@ class Option:
     def ema10(self):
         ema10 = self.data_stock_price
         ema10['EMA_10'] = self.data_stock_price['Close'].ewm(span = 10, adjust=False).mean()
-        print (ema10.tail())  
+        return self.data_stock_price['EMA_10'][self.data_stock_price.index[-1]]
 
     def ema50(self):
         ema50 = self.data_stock_price
-        ema50['EMA_%0'] = self.data_stock_price['Close'].ewm(span = 50, adjust=False).mean()
-        print (ema50.tail())  
+        ema50['EMA_50'] = self.data_stock_price['Close'].ewm(span = 50, adjust=False).mean()
+        return self.data_stock_price['EMA_50'][self.data_stock_price.index[-1]]
 
     def ema200(self):
         ema200 = self.data_stock_price
         ema200['EMA_200'] = self.data_stock_price['Close'].ewm(span=200, adjust=False).mean()
-        print (ema200.tail())  
+        return self.data_stock_price['EMA_200'][self.data_stock_price.index[-1]]  
 
     #def bollinger_bands(self)
 
-    #def relative_strength_index(self)
+    def relative_strength_index(self):
+        def rsi(data, time_period):
+            diff = data.diff().dropna() 
+
+            up_change = 0*diff
+            down_change = 0*diff
+
+            up_change[diff>0]=diff[diff>0]
+            down_change[diff<0]=diff[diff<0]
+
+            avg_up = up_change.ewm(com = time_period-1, min_periods = time_period).mean()
+            avg_down = down_change.ewm(com = time_period-1, min_periods = time_period).mean()
+
+            rs = abs(avg_up/avg_down)
+            rsi = 100 - 100/(1+rs)
+            return rsi
+            
+        self.data_stock_price['RSI'] = rsi(self.data_stock_price['Adj Close'], 14)
+        print(self.data_stock_price['RSI'][self.data_stock_price.index[-1]])
+        
 
     #def momentum(self)
 
@@ -54,11 +73,11 @@ class Option:
         ema_12['EMA_12'] = self.data_stock_price['Close'].ewm(span=12, adjust=False).mean()
         ema_26['EMA_26'] = self.data_stock_price['Close'].ewm(span=26, adjust=False).mean()
         macd['MACD'] = self.data_stock_price['EMA_12']-self.data_stock_price['EMA_26']
-        ema_9['Signal Line For MACD'] = self.data_stock_price['MACD'].ewm(span=9, adjust=False).mean()
+        ema_9['Signal Line'] = self.data_stock_price['MACD'].ewm(span=9, adjust=False).mean()
 
-        if self.data_stock_price['Signal Line For MACD'][self.data_stock_price.index[-1]] > self.data_stock_price['MACD'][self.data_stock_price.index[-1]]:
+        if self.data_stock_price['Signal Line'][self.data_stock_price.index[-1]] > self.data_stock_price['MACD'][self.data_stock_price.index[-1]]:
             return True
-        elif self.data_stock_price['Signal Line For MACD'][self.data_stock_price.index[-1]] == self.data_stock_price['MACD'][self.data_stock_price.index[-1]]:
+        elif self.data_stock_price['Signal Line'][self.data_stock_price.index[-1]] == self.data_stock_price['MACD'][self.data_stock_price.index[-1]]:
             pass
         else:
             return False
@@ -77,6 +96,6 @@ class Option:
         #For example: Close position of {stock_name} at price: {price}
 
 
-o1 = Option("tsla",0,0,False)
+o1 = Option("tsla",0)
 o1.scrape_data()
-o1.MACD()
+o1.relative_strength_index()
