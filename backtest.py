9041c8dd533a646cd7be50f670f102e7bb20fe4b
Teodor Perunicic <teodor.perun@gmail.com>
2021-10-22 10:53:50 +0200

    rsm
    rsm

--- backtest.py
+++ backtest.py
@@ -1,42 +1,81 @@
-import backtrader as bt
-import pandas as pd 
+import yfinance as yf
+import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
-import collections
+import pandas as pd
+import ta
 
-class TestStrategy(bt.Strategy):
-    def log(self, txt, dt=None):
-        dt = dt or self.datas[0].datetime.date(0)
-        print ('%s, %s' % (dt.isoformat(), txt))
+df = yf.download('EURUSD=X', start='2021-08-30', interval='30m')
 
-    def __init__(self):
-        self.dataclose = self.datas[0].close
+
+
+def applytehnicals(df):
+    df['%K'] = ta.momentum.stoch(df.High, df.Low, df.Close, window=14, smooth_window=3)
+    df['%D'] = df['%K'].rolling(3).mean()
+    df['RSI'] = ta.momentum.rsi(df.Close, window=14)
+    df['MACD'] = ta.trend.macd_diff(df.Close)
+
+    df.dropna(inplace=True)
     
-    '''def next():
-        
-    '''
-    def next(self):
-        self.log('Close, %.2f' % self.dataclose[0])
-
-        if self.dataclose[0] < self.dataclose[-1]:
-            if self.dataclose[-1] < self.dataclose[-2]:
-                self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                self.buy()
+applytehnicals(df)
 
 
-cerebro = bt.Cerebro()
-cerebro.broker.set_cash(100000)
 
-data = bt.feeds.YahooFinanceCSVData(
-    dataname = 'TSLA.csv',
-    fromdate = datetime(2000,1,1),
-    todate = datetime(2020,12,31),
-    reverse = False
-)
+def gettriggers(df, lags, buy=True):
+    dfx = pd.DataFrame()
+    for i in range (1,lags+1):
+        if buy:
+            mask = (df['%K'].shift(i)<20) & (df['%D'].shift(i)<20)
+        else:
+            mask = (df['%K'].shift(i)>80) & (df['%D'].shift(i)>80)
+        dfx=dfx.append(mask,ignore_index=True)
+    return dfx.sum(axis=0)
 
-cerebro.adddata(data)
-cerebro.addstrategy(TestStrategy)
+df['BuyTrigger'] = np.where(gettriggers(df,4),1,0)
+df['SellTrigger'] = np.where(gettriggers(df,4,False),1,0)
 
-print ('Strating Portfolio value %f' % cerebro.broker.getvalue())
-cerebro.run()
-print('Ending Portfolio value %f' % cerebro.broker.getvalue())
+df['Buy'] = np.where((df['BuyTrigger']) & (df['%K'].between(20,80)) & (df['%D'].between(20,80)) & (df['RSI']>50) & (df['MACD']>0),1,0)
+df['Sell'] = np.where((df['SellTrigger']) & (df['%K'].between(20,80)) & (df['%D'].between(20,80)) & (df['RSI']<50) & (df['MACD']<0),1,0)
+
+
+
+Buying_dates, Selling_dates = [],[]
+
+for i in range(len(df)-1):
+    if df.Buy.iloc[i]:
+        Buying_dates.append(df.iloc[i+1].name)
+
+        for num,j in enumerate(df.Sell[i:]):
+            if j:
+                Selling_dates.append(df.iloc[num+i+1].name)
+                break
+
+cutit = len(Buying_dates)-len(Selling_dates)
+
+if cutit:
+    Buying_dates = Buying_dates[:-cutit]
+
+frame = pd.DataFrame({'Buying_dates':Buying_dates, 'Selling_dates':Selling_dates})
+
+
+
+actuals = frame[frame.Buying_dates>frame.Selling_dates.shift(1)]
+
+
+
+def profit():
+    Buyprices = df.loc[actuals.Buying_dates].Open
+    Sellingprices = df.loc[actuals.Selling_dates].Open
+    return (Buyprices.values-Sellingprices.values)/Buyprices.values
+                       
+
+profit()
+
+profit().mean()
+
+(profit()+1).prod()
+
+plt.figure(figsize=(20,10))
+plt.plot(df.Close, color='k', alpha=0.7)
+
+plt.scatter(actuals.Buying_dates, df.Open[actuals.Buying_dates], marker='^', color='g', s=500)
+plt.scatter(actuals.Selling_dates, df.Open[actuals.Selling_dates], marker='v', color='r', s=500)
--- trade.py
+++ trade.py
@@ -1,19 +1,92 @@
-import pandas as pd 
-import pandas.io as web
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
 import numpy as np
+import ta
+import time
+
+from binance.client import Client
+
+
+client = Client(api_key, api_secret)
+
+
+def getminutedata(symbol,interval,lookback):
+        frame = pd.DataFrame(client.get_historical_klines(symbol,interval,lookback + 'min ago UTC'))
+
+        frame = frame.iloc[:,:6]
+        frame.columns = ['Time','Open','High','Low','Close','Volume']
+        frame = frame.set_index('Time')
+        frame.index = pd.to_datetime(frame.index, unit='ms')
+        frame = frame.astype(float)
+
+        return frame
+
+
+def applytehnicals(df):
+        df['%K'] = ta.momentum.stoch(df.High, df.Low, df.Close, window=14, smooth_window=3)
+        df['%D'] = df['%K'].rolling(3).mean()
+        df['RSI'] = ta.momentum.rsi(df.Close, window=14)
+        df['MACD'] = ta.trend.macd_diff(df.Close)
+
+        df.dropna(inplace=True)
+
+class Signals:
+        def __init__(self, df, lags):
+                self.df = df
+                self.lags = lags
+
+        def gettrigger(self):
+                dfx = pd.DataFrame()
+
+                for i in range(self.lags+1):
+                        mask = (self.df['%K'].shift(i)<20) and (self.df['%D'].shift(i)<20)
+                        dfx = dfx.append(mask, ignore_index=True)
+                
+                return dfx.sum(axis=0)
+
+        def decide(self):
+                self.df['Trigger'] = np.where(self.gettrigger(),1,0)
+                self.df['BUY'] = np.where((self.df.trigger) and (self.df['%K'].between(20,80)) and (self.df['%D'].between(20,80)) and (self.df.RSI > 50) and (self.df.MACD > 0),1,0)
+
+def strategy(pair,qty, open_position=False):
+        df = getminutedata(pair, '1m', '100')
+
+        applytehnicals(df)
+        inst=Signals(df,25)
+
+        inst.decide()
+
+        if df.BUY.iloc[-1]:
+                order = client.create_order(symbol=pair,side='BUY',type='MARKET', quantity=qty)
+                print(order)
+                buyprice = float(order['fills'][0]['price'])
+                open_position = True
+
+        while open_position:
+                time.sleep(0.5)
+                df = getminutedata(pair, '1m', '2')
+
+                if df['Close'][-1] <= buyprice * 0.995 or df['Close'][-1] >= buyprice*1.005:
+                        order = client.create_order(symbol=pair,side='SELL',type='MARKET', quantity=qty)
+                        break
 
 
 
-stocks = []
 
-'''
-if ((stock.MACD() and stock.price > stock.ema200()) or (stock.bollinger_band()<0 and stock.relative_strength_index()) or (stock.momentum()>-0.2 and stock.volatility()>=1.5 and stock.MACD()>0)) 
-        buy stock
-'''
 
-stocks_for_trading = []
-options = []
-etf = []
-futures = []
+        
+
+
+            
+
+
+
+
+
+
+
+
+            
 
 
